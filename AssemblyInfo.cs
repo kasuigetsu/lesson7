3a1588a4a4938bd517f4c452be83ddc9a48f002f
kasuigetsu <arina_barsukova@bk.ru>
2022-02-23 22:31:04 +0300

    Добавьте файлы проекта.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lesson7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lesson7")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7f95c169-ecd6-483b-8f5b-149fdb940ea2")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: lesson7.Program
+// Assembly: lesson7, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 7F7D3994-9C28-4582-BA6C-9AA61B3AD164
+// Assembly location: C:\Users\Core_i5\source\repos\lesson7\lesson7\bin\Debug\lesson7.exe
+
+using System;
+
+namespace lesson7
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, buddy, enter your password to login!");
+      if (Console.ReadLine() == "peaceful")
+        Console.WriteLine("Come in! I'm glad to see you!");
+      else
+        Console.WriteLine("Step back, you look like mafia!");
+      Console.ReadLine();
+        }
+      
+  }
+}
